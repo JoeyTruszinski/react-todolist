acd1742bf81e7a17087a709e1b76872529c1768a
JoeyTruszinski <78323429+JoeyTruszinski@users.noreply.github.com>
2021-12-13 16:32:09 -0600

    added components folder with basic form input

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Form() {
+  return (
+    <form className="form">
+      <input type="text" className="form-text" />
+      <button className="form-button">Add To List</button>
+    </form>
+  );
+}
+
+export default Form;
