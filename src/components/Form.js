c1a3e3bcec6d1c1eac28bea0e22cbbb2f5a45c0f
JoeyTruszinski <78323429+JoeyTruszinski@users.noreply.github.com>
2021-12-13 17:15:07 -0600

    Created the adding to do item to a list

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Form() {
-  return (
-    <form className="form">
-      <input type="text" className="form-text" />
-      <button className="form-button">Add To List</button>
-    </form>
-  );
+import ToDoList from "./ToDoList";
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: [],
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <ToDoList items={this.state.items} />
+      </div>
+    );
+  }
 }
-
-export default Form;
---
+++ src/components/ToDoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDoList = (props) => (
+  <ul>
+    {props.items.map((item, index) => (
+      <li key={index}>{item}</li>
+    ))}
+  </ul>
+);
+
+export default ToDoList;
